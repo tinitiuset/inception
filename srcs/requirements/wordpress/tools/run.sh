5df9d1eebfebbc50c12eb9024feaf9219febf902
tinitiuset <tinitiuset@gmail.com>
2024-01-12 17:50:55 +0100

    Add Redis and allow fpm as root

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -3,7 +3,16 @@
 wp core download --version=6.0 --allow-root
 wp config create --dbname="$MYSQL_USER" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="mariadb" --allow-root
 wp core install --url="$HOST" --title="$WP_TITLE" --admin_user="$WP_USER" --admin_password="$WP_PASSWORD" --admin_email="$WP_EMAIL" --skip-email --allow-root
+
 wp option update home "https://$HOST"
 wp option update siteurl "https://$HOST"
 
-exec php-fpm81 -F
+wp plugin install redis-cache --activate
+
+wp config set WP_REDIS_HOST "redis"
+wp config set WP_REDIS_PORT "6379"
+wp config set WP_REDIS_DATABASE "1"
+
+wp redis enable
+
+exec php-fpm81 -F -R
