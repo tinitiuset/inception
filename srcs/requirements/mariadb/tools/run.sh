5e86f0c53b6e243c6b2c1d037d3d83c1e2ac4bf9
tinitiuset <tinitiuset@gmail.com>
2024-01-13 16:12:57 +0100

    Add healthcheck to mariadb

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -2,21 +2,17 @@
 
 if [ -d "/run/mysqld" ]; then
 	echo "mysqld exists, skipping creation"
-	chown -R mysql:mysql /run/mysqld
 else
 	echo "mysqld not found, creating...."
 	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld
 fi
 
 if [ -d /var/lib/mysql/mysql ]; then
 	echo "A mariadb database already exists, skipping creation"
-	chown -R mysql:mysql /var/lib/mysql
 else
 	echo "A mariadb database does not exist, creating..."
-	chown -R mysql:mysql /var/lib/mysql
 
-	mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
+	mysql_install_db --user=root --ldata=/var/lib/mysql > /dev/null
 
 	cat << EOF >> "/tmp/users.sql"
 USE mysql;
@@ -36,8 +32,8 @@ GRANT ALL ON \`$MYSQL_USER\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWO
 EOF
 fi
 
-mysqld --user=mysql --bootstrap --verbose=0 < /tmp/users.sql
+mysqld --user=root --bootstrap --verbose=0 < /tmp/users.sql
 
 rm -rf /tmp/users.sql
 
-exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 "$@"
+exec /usr/bin/mysqld --user=root --console --skip-name-resolve --skip-networking=0 "$@"
